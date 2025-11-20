0b671b91bc62c2a32c99521cc3c2049124712825
mux002 <mux002@ucsd.edu>
2025-11-20 12:22:48 -0800

    lab8

--- meta/main.js
+++ meta/main.js
@@ -1,4 +1,5 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+import scrollama from 'https://cdn.jsdelivr.net/npm/scrollama@3.2.0/+esm';
 
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
@@ -435,3 +436,55 @@ function updateFileDisplay(filteredCommits) {
     }
 
 updateFileDisplay(filteredCommits);
+
+d3.select('#scatter-story')
+    .selectAll('.step')
+    .data(commits)
+    .join('div')
+    .attr('class', 'step')
+    .html(
+        (d, i) => `
+            On ${d.datetime.toLocaleString('en', {
+        dateStyle: 'full',
+        timeStyle: 'short',
+        })},
+            I made <a href="${d.url}" target="_blank">${
+        i > 0 ? 'another glorious commit' : 'my first commit, and it was glorious'
+        }</a>.
+            I edited ${d.totalLines} lines across ${
+        d3.rollups(
+            d.lines,
+            (D) => D.length,
+            (d) => d.file,
+        ).length
+        } files.
+            Then I looked over all I had made, and I saw that it was very good.
+        `,
+    );
+
+function onStepEnter({ element }) {
+    const commit = element.__data__;                
+    const t = commit.datetime;
+    commitProgress = timeScale(t);                 
+    progressEl.value = Math.round(commitProgress);   
+    timeEl.textContent = t.toLocaleString('en', { dateStyle:'long', timeStyle:'short' });
+
+    const commitMaxTime = t;
+    const filteredCommits = commits.filter(d => d.datetime <= commitMaxTime);
+    const filteredData    = data.filter(d => new Date(d.datetime) <= commitMaxTime);
+
+    updateScatterPlot(data, filteredCommits);
+    updateCommitInfo(filteredData, filteredCommits);
+    updateFileDisplay(filteredCommits);
+    }
+
+const scroller = scrollama();
+scroller
+    .setup({
+        container: '#scrolly-1',
+        step: '#scrolly-1 .step',
+        offset: 0.6,     
+        debug: false
+    })
+    .onStepEnter(onStepEnter);
+window.addEventListener('resize', scroller.resize);
